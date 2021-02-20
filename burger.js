6482df6cba22eba19af7c22276f9fc10041d1b14
3nergetik <75131588+3nergetik@users.noreply.github.com>
2021-02-20 14:41:08 +0500

    Update burger.js

--- burger.js
+++ burger.js
@@ -2,7 +2,7 @@ const headerBurger = document.querySelector('.headerBurger');
 const headerMenu = document.querySelector('.headerMenu');
 const body = document.querySelector('body');
 
-headerBurger.onclick = function() {
+headerBurger.onclick = function () {
     headerBurger.classList.toggle('active');
     headerMenu.classList.toggle('active');
     body.classList.toggle('lock');
