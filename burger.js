920b22dbb6d6becc2418df1bc26b8f6cf38c646f
3nergetik <75131588+3nergetik@users.noreply.github.com>
2021-03-16 15:44:40 +0500

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,9 +1,13 @@
 const headerBurger = document.querySelector('.header-burger');
 const headerMenu = document.querySelector('.header-menu');
 const body = document.querySelector('body');
+const pcInfo = documen.querySelector('.pcinfo');
+const dropdownActions = document.querySelector('.dropdown-actions');
 
 headerBurger.onclick = function () {
     headerBurger.classList.toggle('open-menu');
     headerMenu.classList.toggle('active');
     body.classList.toggle('lock');
 }
+
+dropdownActions.innerHTML = pcInfo.parentNode.innerHTML;
