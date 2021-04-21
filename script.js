0a92933e475164da741d01af39f2251117aec4b5
3nergetik <75131588+3nergetik@users.noreply.github.com>
2021-04-22 00:23:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
 const headerBurger = document.querySelector('.header-burger');
-const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup-close');
+const popupWrapper = document.querySelector('.popup-wrapper');
 const sideButton = document.querySelector('.side-button');
 const nav = document.querySelector('nav');
 const body = document.querySelector('body');
 const headerMenu = document.querySelector('.header-menu');
 const headerBody = document.querySelector('.header-body');
 
-const delay_popup = 5000;
-
-const showModal = () => {
-  popup.classList.add('active');
+popupClose.onclick = () => {
+  popupWrapper.classList.add('hide');
 }
-
-/* setTimeout(showModal, delay_popup); */
 
 headerBurger.onclick = () => {
   nav.classList.toggle('show-menu');
   headerMenu.classList.toggle('show-menu-item');
   headerBurger.classList.toggle('open-menu');
   headerBody.classList.toggle('show-item');
-}
-
-popupClose.onclick = () => {
-  popup.classList.remove('active');
+  body.classList.toggle('lock');
 }
 
 sideButton.onclick = (evt) => {
   evt.preventDefault();
   popup.classList.remove('active');
 }
-
-console.log(headerBody);
